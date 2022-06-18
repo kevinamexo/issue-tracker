10e84a9279c721dbc8f092de80b1a8bcba3115e6
kevinamexo <bangitskev@gmail.com>
2022-06-18 21:14:31 +0100

    fix: fix basic test to test github action

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders logo", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logo = screen.getByText(/logo/i);
+  expect(logo).toBeInTheDocument();
 });
